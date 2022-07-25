b96feea1f402df266eb8ef569d2b08fba99eabeb
Zach DeLand <zrdeland@gmail.com>
2022-07-25 10:00:09 -0400

    Fixed array rendering

--- src/Features/Boon_Meter/Boon-List/Boon-List.tsx
+++ src/Features/Boon_Meter/Boon-List/Boon-List.tsx
@@ -1,18 +1,79 @@
 import Boon from "../Boon/Boon";
 import IBoonProps from "../Boon/BoonInterface";
 
-export interface IBoonListProps {
-  boonList: React.ReactElement[];
+// export interface IBoonListProps {
+//   boonList: json[];
+// }
+
+interface ICard {
+  name: string;
+  image: string;
+  id: number;
 }
 
-export default function BoonList (props: IBoonListProps) {
+interface IProps {
+  card: IBoonProps;
+  key: string;
+}
+
+const cards: IBoonProps[] = [
+  {
+    id: "1",
+    name: "ZRD Mega Nuke",
+    god: "ZRD",
+    weapon_category: "attack",
+    buff_category: "damage",
+    damage_modifier: 1000
+  },
+  {
+    id: "2",
+    name: "ZRD Wet Noodle",
+    god: "ZRD",
+    weapon_category: "attack",
+    buff_category: "damage",
+    damage_modifier: 50
+  },
+  {
+    id: "3",
+    name: "ZRD Whimpy Whistle",
+    god: "ZRD",
+    weapon_category: "call",
+    buff_category: "support",
+    damage_modifier: 0
+  },
+];
+
+const Card: React.FC<{ card: IBoonProps }> = ({ card }) => {
   return (
-    <div>
-      <ul>
-        {props.boonList}
-      </ul>
-    </div>
-  );
+    <tr>
+      <td>
+      {card.name}: {card.weapon_category} 
+      </td>
+      <td>
+        {card.damage_modifier}
+      </td>
+    </tr>
+  )
+  
+};
+
+export default function BoonList() {
+
+    return (<>
+      <table>
+      <thead>
+        <tr>
+            <td>Boon</td>
+            <td>Damage Modifier</td>
+        </tr>
+        </thead>
+        <tbody>
+        {cards.map(card => {
+          return <Card key={card.id} card={card} />;
+        })}
+        </tbody>
+      </table>
+    </>)
 }
 
  
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -6,7 +6,7 @@ import BoonList from '../../Features/Boon_Meter/Boon-List/Boon-List';
 import Boon from '../../Features/Boon_Meter/Boon/Boon'
 
 import { useAppSelector } from '../../App/hooks'
-import { JsxElement } from 'typescript';
+
 
 
 function HomePage() {
@@ -21,26 +21,19 @@ function HomePage() {
 
   // console.log("BOONS: ", boons)
 
-  const boons = [<Boon id={'zach-id'} name={'ZRD Mega Nuke'} god={'ZRD'} weapon_category={'attack'} buff_category={'damage'} damage_modifier={1000} />]
-
+ 
   return (
     <div className="App">
       <header className="App-header">
         <h2>Hades Boon Power Meter</h2>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
         <img src={charon_obol} className="rotate-img" alt="logo" />
         <br />
         <br />
         <br />
         <br />
-        <BoonList boonList={boons} />
+        <BoonList />
         
-
-        {/* <BoonList boonList={[
-          <Boon id={'zach-id'} name={'ZRD Mega Nuke'} god={'ZRD'} weapon_category={'attack'} buff_category={'damage'} damage_modifier={1000} />,
-          <Boon id={'zach-id'} name={'ZRD Slippery Noodle'} god={'ZRD'} weapon_category={'Special'} buff_category={'damage'} damage_modifier={300} />,
-          <Boon id={'zach-id-2'} name={'ZRD Whimpy Whistle'} god={'ZRD'} weapon_category={''} buff_category={'support'} damage_modifier={-100} />
-        ]} /> */}
         </header>
     </div>
   );
